d780e540f459a62b7db5c3db793bac4289ba2c96
JamesPImes <jamesimes@gmail.com>
2024-07-31 01:08:07 -0600

    clean/load working dataset

---
+++ preprocess/clean_working_dataset.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+import dotenv
+
+from shl_bhl.lat_long import (
+    fill_lat_long_midpoints,
+    fill_lateral_distances,
+)
+from shl_bhl.wells_cull import (
+    default_load_and_cull,
+    cull_by_twprge,
+)
+
+dotenv.load_dotenv()
+RAW_DATA_DIR = Path(os.getenv('RAW_DATA_DIR'))
+WORKING_DATA_DIR = Path(os.getenv('WORKING_DATA_DIR'))
+WORKING_DATA_FN = Path(os.getenv('WORKING_DATA_FN'))
+
+__all__ = [
+    'clean_working_dataset',
+]
+
+
+def clean_working_dataset():
+    wells = default_load_and_cull()
+    keep_twprge = [
+        '7n60w',
+        '7n59w',
+        '8n59w',
+        '8n60w',
+        '9n58w',
+        '9n59w',
+        '9n60w',
+        '10n58w',
+        '10n57w',
+    ]
+    wells = cull_by_twprge(wells, keep_twprge)
+    wells = fill_lat_long_midpoints(wells)
+    wells = fill_lateral_distances(wells)
+    wells.to_csv(WORKING_DATA_DIR / WORKING_DATA_FN, index=False)
+
+
+if __name__ == '__main__':
+    clean_working_dataset()
---
+++ preprocess/load_working_dataset.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+import dotenv
+import pandas as pd
+
+dotenv.load_dotenv()
+WORKING_DATA_DIR = Path(os.getenv('WORKING_DATA_DIR'))
+WORKING_DATA_FN = Path(os.getenv('WORKING_DATA_FN'))
+
+__all__ = [
+    'load_working_dataset',
+]
+
+
+def load_working_dataset(fp: Path = WORKING_DATA_DIR / WORKING_DATA_FN):
+    return pd.read_csv(fp, parse_dates=['Spud_Date', 'Stat_Date'])
+
+
+df = load_working_dataset()
+print(df)
