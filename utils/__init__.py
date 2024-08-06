b034688b6e5ae08af43323d88e2d269fb3caa9da
JamesPImes <jamesimes@gmail.com>
2024-08-05 18:23:41 -0600

    feat: load parameters from json

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,7 @@
 from .utils import (
     get_prod_window,
 )
+from .json_to_model_params import (
+    json_to_exp_regress_params,
+    json_to_nearest_wells_params,
+)
---
+++ utils/json_to_model_params.py
@@ -0,0 +1,38 @@
+import json
+
+__all__ = [
+    'json_to_exp_regress_params',
+    'json_to_nearest_wells_params',
+]
+
+
+def json_to_exp_regress_params(fp):
+    with open(fp, 'r') as json_file:
+        model_params = json.load(json_file)
+    kwarg_sets = {}  # For use of params as kwargs.
+    for param_set in model_params:
+        model_name = param_set['model_name']
+        # Convert raw params to kwargs for ExpRegressionModel init
+        exp_params = {
+            'min_months': param_set['min_months'],
+            'max_months': param_set['max_months'],
+            'discard_gaps': param_set['discard_gaps'],
+            'weight_function': lambda v: v ** param_set['weight_power'],
+        }
+        kwarg_sets[model_name] = exp_params
+    return kwarg_sets
+
+
+def json_to_nearest_wells_params(fp):
+    with open(fp, 'r') as json_file:
+        model_params = json.load(json_file)
+    kwarg_sets = {}
+    for param_set in model_params:
+        model_name = param_set['model_name']
+        # Convert raw params to kwargs for nearest wells parameters
+        nearest_wells_params = {
+            'k': param_set['k'],
+            'distance_weighting': param_set['distance_weighting'],
+        }
+        kwarg_sets[model_name] = nearest_wells_params
+    return kwarg_sets
