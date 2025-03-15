0232d7cea883557bea6a2367febea4fe60054223
James Imes <jamesimes@gmail.com>
2025-03-15 15:43:57 -0600

    feat: get cumulative production at certain checkpoints

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@
 from .utils import (
     get_prod_window,
     get_cumulative_days,
+    get_cumulative_production,
 )
 from .json_to_model_params import (
     json_to_exp_regress_params,
--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 __all__ = [
     'get_prod_window',
     'get_cumulative_days',
+    'get_cumulative_production',
 ]
 
 
@@ -57,3 +58,47 @@ def get_cumulative_days(prod_records: pd.DataFrame) -> pd.Series:
     """
     monthly_days = prod_records['calendar_days']
     return pd.Series(range(1, sum(monthly_days) + 1))
+
+def get_cumulative_production(
+        prod_records: pd.DataFrame,
+        predicted_bbls: pd.Series = None,
+        month_checkpoints=(24, 36, 48),
+) -> (dict, dict):
+    """
+    Get cumulative produced BBLs (both actual and predicted) at any
+    number of time frames (e.g., first 24 months, 36 months, 48 months).
+
+    :param prod_records: A dataframe of preprocessed actual monthly
+     production.
+    :param predicted_bbls: A series of predicted daily BBLs (i.e., the
+     output of a prediction model). Should be at least the same length
+     of time as covered by ``prod_records`` (which is actual monthly
+     production).
+    :param month_checkpoints: A list of months to extract. Defaults to
+     the first  24, 36, and 48 months. (Will also always include the
+     maximum number of months in the ``prod_records`` dataframe.)
+    :return: Two dicts, the first being the total actual production at
+     the end of each requested period; the second being the total
+     predicted production at the end of each requested period. The final
+     month will also be included in each dict. If the requested month is
+     greater than the months in the ``prod_records`` dataframe, then
+     that entry in each returned dict will have a ``None`` value.
+    """
+    month_checkpoints = list(month_checkpoints)
+    total_months = len(prod_records)
+    if total_months not in month_checkpoints:
+        month_checkpoints.append(total_months)
+    actual = {}
+    pred = {}
+    for n_months in month_checkpoints:
+        if n_months > total_months:
+            actual[n_months] = None
+            pred[n_months] = None
+            continue
+        actual_subset = prod_records.iloc[:n_months]
+        cumul_days = sum(actual_subset['calendar_days'])
+        actual[n_months] = sum(actual_subset['Oil Produced'])
+        if predicted_bbls is not None:
+            pred_subset = predicted_bbls.iloc[:cumul_days]
+            pred[n_months] = sum(pred_subset)
+    return actual, pred
